b580791e00e3115b29d506534d4a859f37d8c32c
ApesZhao <83993205+ApesZhao@users.noreply.github.com>
2022-04-26 11:19:19 +1000

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,11 @@
+//console.log('Hello');
+//alert('yo!');
+
+//variables
+var b= 'banana';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML="hello JS!";
